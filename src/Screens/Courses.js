eac98aac41b89e11f64ea83ca792e8d5f8e3cbcb
Sagar Das <sagarrdass@gmail.com>
2024-05-07 01:37:13 +0530

    temporarily removed tailwind css property from index.css for rendering issue

--- src/Screens/Courses.js
+++ src/Screens/Courses.js
@@ -6,13 +6,12 @@ function Courses(){
         <p>With the emergence of AI, IT companies are using more and more AI based solutions in their products.</p>
         <p>Hence it is very important for Freshers to gather decent amount of knowledge to get a chance to work for the companies.</p>
         <p>Let's decode the modern preparation strategy and identify the challenges there</p>
-        
-        <ul>
+        <ol>
             <li>Preparation for different Career Tracks</li>
             <li>Full Stack Developer</li>
             <li>Data Scientist or Data Analyst</li>
             <li>Machine Learning Engineer</li>
-        </ul>
+        </ol>
         <p>To prepare well for the Technical Interviews, currently it is not enough to know only one programming language</p>
         <p>Hence following is the preferred list of programming languages which I personally feel is necessary to learn</p>
         <h1>Important Programming Languages</h1>
